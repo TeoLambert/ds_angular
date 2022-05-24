85b3fcd5c1a49c858c2469c1736fecd16ae8b5c8
LAMBERT Teo INNOV/IT-S <BQSN1129@orange.com>
2022-05-24 12:44:57 +0200

    Exercice 6

--- ds_angular/src/app/app.component.ts
+++ ds_angular/src/app/app.component.ts
@@ -10,18 +10,36 @@ import { Statistique } from './models/statistique';
 
 export class AppComponent {
   title = 'ds_angular';
+  stats: Statistique[] = [];
 
-  stat1 : Statistique = {
-    id:"id1",
-    titre:"Statistique n°1",
-    valeur:"Valeur 1"
-  }
+  constructor(){
 
-  stat2 : Statistique = {
+    let stat1 : Statistique = {
+      id:"id1",
+      titre:"Statistique n°1",
+      valeur:"Valeur 1"
+    }
+
+    let stat2 : Statistique = {
     id:"id2",
     titre:"Statistique n°2",
     valeur:"Valeur 2"
+    }
+
+    let stat3 : Statistique = {
+      id:"id3",
+      titre:"Statistique n°3",
+      valeur:"Valeur 3"
+      }
+
+    this.stats.push(stat1, stat2);
+
+    setTimeout(() =>{ this.stats.push(stat3) }, 5000);
+
   }
+  
+
+  
 
 
 }

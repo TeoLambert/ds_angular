b8d3ef31667f747921c39f166ab09062ba6f39d2
LAMBERT Teo INNOV/IT-S <BQSN1129@orange.com>
2022-05-24 12:20:32 +0200

    Exercice 4

--- ds_angular/src/app/app.component.ts
+++ ds_angular/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Statistique } from './models/statistique';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'ds_angular';
+
+  stat1 : Statistique = {
+    id:"id1",
+    titre:"Statistique n°1",
+    valeur:"Valeur 1"
+  }
+
+  stat2 : Statistique = {
+    id:"id2",
+    titre:"Statistique n°2",
+    valeur:"Valeur 2"
+  }
+
+
 }
